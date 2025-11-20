705777591feaf3a55d342607ed6f378a98e1cc13
crigca <crigca@hotmail.com>
2025-11-19 22:23:12 -0300

    Add vibraApp URL to CORS allowedOrigins

--- api/src/main.ts
+++ api/src/main.ts
@@ -27,9 +27,7 @@ async function bootstrap() {
     'http://localhost:5173',
     'http://localhost:5174',
     'https://vibra-kohl.vercel.app',
-    
-   
-
+    'https://vibra-app-ten.vercel.app',
     process.env.FRONTEND_URL,
   ].filter(Boolean) as string[]; // Elimina valores undefined/null
 
